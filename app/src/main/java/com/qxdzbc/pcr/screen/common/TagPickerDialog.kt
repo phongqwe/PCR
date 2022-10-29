5566abc83fc4309b317acd6dcbd03dc5f7abcf9e
qxdzbc <31875822+qxdzbc@users.noreply.github.com>
2022-10-29 11:39:46 +0700

    update: - change calendar icon

---
+++ app/src/main/java/com/qxdzbc/pcr/screen/common/TagPickerDialog.kt
@@ -0,0 +1,7 @@
+package com.qxdzbc.pcr.common
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TagPickerDialog() {
+}
--- app/src/main/java/com/qxdzbc/pcr/screen/entry_creation/EntryCreationScreen.kt
+++ app/src/main/java/com/qxdzbc/pcr/screen/entry_creation/EntryCreationScreen.kt
@@ -1,17 +1,17 @@
 package com.qxdzbc.pcr.screen.main_screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.qxdzbc.pcr.R
+import com.qxdzbc.pcr.common.MBox
 import com.qxdzbc.pcr.common.Ms
 import com.qxdzbc.pcr.common.StateUtils.ms
 import com.qxdzbc.pcr.screen.common.InputField
@@ -29,7 +29,7 @@ fun EntryCreationScreen(
     val dateMs: Ms<Date> = remember { ms(Date()) }
     val openDatePickerMs = remember { ms(false) }
     Surface {
-        Column(modifier = modifier) {
+        Column(modifier = modifier.padding(horizontal =10.dp)) {
             OutlinedTextField(
                 value = DateUtils.displayDateFormat.format(dateMs.value),
                 modifier = Modifier.fillMaxWidth(),
@@ -43,7 +43,7 @@ fun EntryCreationScreen(
                         onClick = { openDatePickerMs.value = true },
                     ) {
                         Icon(
-                            painter = painterResource(id = R.drawable.ic_outline_calendar_today_24),
+                            painter = painterResource(id = R.drawable.calendar_24),
                             contentDescription = "pick date",
                             tint = MaterialTheme.colors.primary
                         )
